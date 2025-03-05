97cd317c220888798fffff1f158a00b4baf78ea0
aiden cox <cox.378@wright.edu>
2025-03-04 19:46:01 -0500

    ints and doubles

--- main.cpp
+++ main.cpp
@@ -9,7 +9,27 @@ int main() {
      * multiline comment
      *
      */
+
     std::cout << "I like pizza" << '\n';
     std::cout << "it's really good!" << '\n';
-    return 0;
+
+
+//ints and data types
+
+    //integer (whole num)
+    int age = 20;
+    int year = 2025;
+    int days = 45;
+
+    std::cout << age << '\n';
+
+    //double (num w decimal)
+    double price = 10.99;
+    double gpa = 4.00;
+    double temperature = 65.01;
+
+    std::cout << price << '\n';
+
+
+return 0;
 }
